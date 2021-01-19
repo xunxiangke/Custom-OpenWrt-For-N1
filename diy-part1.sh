1d6ed6e88bcc39c43326835bad752fcb42fc3ff2
xunxiangke <1539173762a@gmail.com>
2021-01-19 17:10:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#Add custom package
+git clone https://github.com/tuanqing/install-program package/install-program
